214be4cf4c7c1edadd7c30078d8959b93382ca43
adlzanchetta <adlzanchetta@gmail.com>
2022-11-04 09:41:24 -0400

    Includes env into .gitignore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/Layout"
 
+import { base_url } from "../env/globals"
+
 export default function Home() {
   return (
     <Layout>
       <div className="container text-center">
         <div>Hello world!</div>
-        <img src="/me/faceFocusWaterpixels.png" alt="me" />
+        <img src={`${base_url}imgs/faceFocusWaterpixels.png`} alt="andres_face" />
       </div>
     </Layout>
   )
