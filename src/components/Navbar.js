ea47808d12981953d5965a52774a9eb05895cbf3
adlzanchetta <adlzanchetta@gmail.com>
2022-11-04 01:29:07 -0400

    Fix link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,13 +17,12 @@ export default function Navbar() {
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ms-auto">
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/me/portifolio">Portfolio</a></li>
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/me/cv">C.V.</a></li>
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/me/contact">Contact</a></li>
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/portifolio">Portfolio</a></li>
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/cv">C.V.</a></li>
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/contact">Contact</a></li>
                 </ul>
             </div>
         </div>
-        <hl />
     </nav>
   )
 }
