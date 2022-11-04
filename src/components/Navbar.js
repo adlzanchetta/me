28b80be75ac983dbc5a96888d5cbb923c7ce5a8d
adlzanchetta <adlzanchetta@gmail.com>
2022-11-04 09:11:49 -0400

    Remove / from URLs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,9 +15,9 @@ export default function Navbar() {
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
               <ul className="navbar-nav ms-auto">
-                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/portfolio">Portfolio</a></li>
-                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/cv">C.V.</a></li>
-                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/contact">Contact</a></li>
+                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="portfolio">Portfolio</a></li>
+                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="cv">C.V.</a></li>
+                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="contact">Contact</a></li>
                 <li className="nav-item mx-0 mx-lg-1 text-uppercase"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#">En</a></li>
               </ul>
             </div>
